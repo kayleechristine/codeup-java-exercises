34b591312fffb86014f5b388231059b180a45bf8
Kaylee Morris <kaylee.christine.morris@gmail.com>
2022-12-08 14:33:10 -0600

    finished the first part of the java hello world exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        // Renaming the main method prevents us from being able to compile the code.
+        System.out.print("Hello, ");
+        System.out.print("world!");
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    //main method shortcut
+    //main(tab/enter)
+    //psvm(tab/enter)
+
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+        //System.out.println shortcut
+        //sout
+    }
+
+}
