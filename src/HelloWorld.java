5841cee29e8c152afb10075694cdceb775021f65
Kaylee Morris <kaylee.christine.morris@gmail.com>
2022-12-12 12:13:22 -0600

    finished the hello world exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
+
         System.out.println("Hello, world!");
         // Renaming the main method prevents us from being able to compile the code.
         System.out.print("Hello, ");
