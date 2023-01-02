c6e074e80b4a539b75a789b20806ed525a229f31
Kaylee Morris <kaylee.christine.morris@gmail.com>
2023-01-02 15:51:41 -0600

    finished the high & low exercise

--- src/HighLow.java
+++ src/HighLow.java
@@ -7,23 +7,31 @@ public class HighLow {
 
         Random roll = new Random();
         int rollResult = roll.nextInt((100 - 1) + 1) + 1;
-        System.out.println(rollResult);
+        // System.out.println(rollResult);
         int userGuess;
+        int numGuesses = 0;
 
         do {
+            numGuesses += 1;
             Scanner scanner = new Scanner(System.in);
             System.out.print("Guess a number between 1 and 100: ");
             String userInput = scanner.nextLine();
             userGuess = Integer.parseInt(userInput);
 
             if (userGuess < rollResult) {
-                System.out.println("**** HIGHER");
-            } else
-                System.out.println("**** LOWER");
+                System.out.printf("**** HIGHER\n**** %d Guess(es) Remaining\n", 10 - numGuesses);
+            } else if (userGuess > rollResult) {
+                System.out.printf("**** LOWER\n**** %d Guess(es) Remaining\n", 10 - numGuesses);
+            }
 
-        } while (userGuess != rollResult);
+        } while (userGuess != rollResult && numGuesses < 10);
 
-        System.out.println("**********************************\nCongratulations! You won the game!\n**********************************");
+        if (userGuess == rollResult) {
+            System.out.println("**********************************\nCongratulations! You won the game!\n**********************************");
+        } else {
+            System.out.println("**********************************\nYou lost... Better luck next time.\n**********************************");
+        }
+
         return userGuess;
 
     }
