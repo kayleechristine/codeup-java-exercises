d42382542448475d7ed4d116a2cdde38a47208e2
Kaylee Morris <kaylee.christine.morris@gmail.com>
2023-01-04 16:14:28 -0600

    finished the shapes exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -9,14 +9,19 @@ public class Rectangle {
         this.length = length;
         this.width = width;
     }
+
     public double getArea() {
         double area = this.length * this.width;
+        System.out.println("Rectangle method is running...");
         System.out.printf("Area: %s\n", area);
         return area;
     }
+
     public double getPerimeter() {
         double perimeter = (this.length * 2) + (this.width * 2);
+        System.out.println("Rectangle method is running...");
         System.out.printf("Perimeter: %s\n", perimeter);
         return perimeter;
     }
+
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -8,4 +8,18 @@ public class Square extends Rectangle {
         this.side = side;
     }
 
+    public double getArea() {
+        double area = Math.pow(side, 4);
+        System.out.println("Square method is running...");
+        System.out.printf("Area: %s\n", area);
+        return area;
+    }
+
+    public double getPerimeter() {
+        double perimeter = side * 4;
+        System.out.println("Square method is running...");
+        System.out.printf("Perimeter: %s\n", perimeter);
+        return perimeter;
+    }
+
 }
