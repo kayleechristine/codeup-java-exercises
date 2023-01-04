363438fe879a0c48d77aca163e6cc21a0f8e0cee
Kaylee Morris <kaylee.christine.morris@gmail.com>
2023-01-04 16:04:07 -0600

    working on the shapes exercise

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,22 @@
+package shapes;
+
+public class Rectangle {
+    protected double length;
+    protected double width;
+
+    public Rectangle(double length, double width) {
+        System.out.println("A new Rectangle was created.");
+        this.length = length;
+        this.width = width;
+    }
+    public double getArea() {
+        double area = this.length * this.width;
+        System.out.printf("Area: %s\n", area);
+        return area;
+    }
+    public double getPerimeter() {
+        double perimeter = (this.length * 2) + (this.width * 2);
+        System.out.printf("Perimete: %s\n", perimeter);
+        return perimeter;
+    }
+}
---
+++ src/shapes/Square.java
@@ -0,0 +1,11 @@
+package shapes;
+
+public class Square extends Rectangle {
+    protected double side;
+
+    public Square(double side) {
+        super(side, side);
+        this.side = side;
+    }
+
+}
